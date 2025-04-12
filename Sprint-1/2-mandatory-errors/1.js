7fb93a8a1cf1aaf6e9fb3623722e0e9b8d1c0259
sripathman <sripathmann@gmail.com>
2025-04-12 13:17:52 +0100

    correction sprint-1

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,8 +1,8 @@
 // trying to create an age variable and then reassign the value by 1
-
+/*
 const age = 33;
 age = age + 1;
-/*  In JavaScript (and many other languages), const declares a constant,
+  In JavaScript (and many other languages), const declares a constant,
  meaning its value cannot be changed after it's initially assigned. */
  let age = 33; 
 age = age + 1; 
