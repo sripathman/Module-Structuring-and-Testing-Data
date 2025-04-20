3cea1414464390903db08b534498ce93e09487f4
sripathman <sripathmann@gmail.com>
2025-04-20 14:31:49 +0100

    add folder Sprint-1

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,6 @@
-This is just an instruction for the first activity - but it is just for human consumption
+
+/*This is just an instruction for the first activity - but it is just for human consumption
 We don't want the computer to run these 2 lines - how can we solve this problem?
+*/
+//Answer :
+//We can comment out these two lines by adding // in front of them so that computer skips these lines
